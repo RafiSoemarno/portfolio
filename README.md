77c8b2efbb9ce28af59ed628395163e55d535117
RafiSoemarno <soemarnorafi@gmail.com>
2021-11-19 15:39:18 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Personal Portfolio
+
+My personal portfolio using Svelte and Tailwind, deployed on Vercel.
---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<main>
+	<div>
+		
+	</div>
+	<div>
+		<h1>Bismani Rafi Lusadi Soemarno</h1>
+		<p>Hello!</p>
+	</div>
+</main>
+
+<style lang="postcss" global>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
