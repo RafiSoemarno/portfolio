580100c9cbd477fb4136173651cbc33523eed421
RafiSoemarno <soemarnorafi@gmail.com>
2021-11-20 11:50:37 +0700

    test commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
 	import Skill from './component/Skill.svelte';
-	import skills from './skills.json';
+	import skills from './data/skills.json';
 </script>
 
-<main class="bg-primary py-16">
+<main class="bg-primary text-gray-300 py-16">
 	<div class="flex flex-row justify-center gap-12">
-		<div class="flex flex-col">
+		<div class="flex flex-col gap-5">
 			<img src="https://via.placeholder.com/500x700" alt="" />
+			<p class="text-center">Likes cats. Enjoys learning about new development tools that make life easier.</p>
+			<hr>
+			<h2 class="text-3xl text-center">Portfolio</h2>
+			<div class="grid grid-cols-2 gap-5">
+
+			</div>
 		</div>
-		<div class="flex flex-col text-gray-300 gap-5">
+		<div class="flex flex-col gap-5">
 			<h1 class="text-4xl">Bismani Rafi Lusadi Soemarno</h1>
 			<hr>
 			<div class="flex items-center gap-2 text-2xl">
@@ -33,6 +39,11 @@
 				{#each skills as skill}
 					<Skill icon={skill.icon} name={skill.name} level={skill.level}/>
 				{/each}
+			</div>
+			<hr>
+			<h2 class="text-3xl text-center">Achievements</h2>
+			<div class="grid grid-cols-2 gap-5">
+
 			</div>
 		</div>
 	</div>
---
+++ src/component/Achievement.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let icon;
+    export let name;
+    export let level;
+    let dots = [];
+
+    for (let i = 0; i < 5; i++) {
+        dots[i] = i < level ? true : false;
+    }
+</script>
+
+<div class="flex justify-between gap-4 bg-white bg-opacity-10 rounded-full p-4 px-6">
+    <div>
+        <i class={icon} />
+        <span class="ml-3 font-bold">{name}</span>
+    </div>
+    <div class="flex gap-1 items-center">
+        {#each dots as dot}
+            {#if dot}
+                <span class="material-icons-round">circle</span>
+            {:else}
+                <span class="material-icons-outlined">circle</span>
+            {/if}
+        {/each}
+    </div>
+</div>
+
+<style>
+    .material-icons-round {
+        font-size: 16px;
+        vertical-align: middle;
+    }
+    .material-icons-outlined {
+        font-size: 16px;
+        vertical-align: middle;
+    }
+</style>
